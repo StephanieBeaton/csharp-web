14993aeef37e59c6cae8ea9f6c6f158fba37dcf9
sabet <sabet@cstructor.com>
2017-04-06 20:58:36 -0700

    MVC HelloWorld - Class Lecture

---
+++ MyFullMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyFullMVC.MvcApplication" Language="C#" %>
---
+++ MyFullMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyFullMVC.Startup))]
+namespace MyFullMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
